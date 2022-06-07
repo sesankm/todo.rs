63679b02eac7384bae624b5af0a8901ed044808c
sesankm <sesank.mallikarjuna@gmail.com>
2022-06-07 13:00:18 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,167 +1,16 @@
-use std::fs;
-use std::fs::File;
 use std::io::{stdin, stdout, Write};
-use std::str::Split;
 
-use termion::color;
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
 
-fn read_file() -> TaskList {
-	let mut tl: TaskList = TaskList::new();
-	let contents = fs::read_to_string("/home/sesank/.tasks");
-
-	match contents {
-		Err(_) => return tl,
-		_ => {}
-	}
-
-	let contents = contents.unwrap();
-
-	let tasks_raw: Split<&str> = contents.split("\n");
-
-	for task in tasks_raw {
-		let mut task_details: Split<&str> = task.split(",");
-
-		let text = task_details.next();
-		let completion = task_details.next();
-
-		if text == None || completion == None {
-			break;
-		}
-
-		tl.add_task(Task::new(text.unwrap().to_string(), completion.unwrap().to_string() == "1".to_string()));
-	}
-	tl
-}
-
-struct Task {
-	text: String,
-	completed: bool
-}
-
-
-impl Task {
-	fn new(name: String, completed: bool) -> Task {
-		Task {
-			text: name,
-			completed: completed
-		}
-	}
-
-	fn toggle_status(&mut self) {
-		self.completed = !self.completed;
-	}
-
-	fn display(&self, row:i32, selected:bool) {
-		match self.completed {
-			true => print!("[X] "),
-			false => print!("[ ] ")
-		}
-		match selected {
-			true => print!("{}{}", color::Fg(color::Black), color::Bg(color::White)),
-			false => print!("{}{}", color::Fg(color::White), color::Bg(color::Black))
-		}
-		println!("{}{}{}", self.text, termion::style::Reset, termion::cursor::Goto(1, (row + 1) as u16))
-	}
-
-}
-
-struct TaskList {
-	selected: i32,
-	todos: Vec<Task>
-}
-
-impl TaskList {
-	fn new() -> TaskList {
-		TaskList {
-			selected: 0,
-			todos: Vec::new()
-		}
-	}
-
-	fn display(&self) {
-		print!("{}", termion::clear::All);
-		print!("{}", termion::cursor::Hide);
-		print!("{}", termion::cursor::Goto(1, 1));
-
-		if self.todos.len() as i32 == 0 {
-			println!("No todo items.");
-		}
-		for i in 0..self.todos.len() as i32 {
-			self.todos[i as usize].display(i, i == self.selected);
-		}
-
-		show_controls(self.todos.len() as i32);
-	}
-
-	fn add_task(&mut self, task: Task) {
-		self.todos.push(task);
-	}
-
-	fn inc(&mut self) {
-		if self.selected + 1 < self.todos.len() as i32 {
-			self.selected += 1;
-		}
-	}
-
-	fn dec(&mut self){
-		if self.selected - 1 >= 0 {
-			self.selected -= 1;
-		}
-	}
-
-	fn remove_selected(&mut self){
-		if self.todos.len() > 0 {
-			self.todos.remove(self.selected as usize);
-		}
-
-		if self.todos.len() == 0 {
-			return;
-		}
-		else if (self.selected as usize) >= self.todos.len() {
-			self.selected = (self.todos.len() - 1) as i32;
-		}
-	}
-
-	fn dump(&self){
-		let mut file = File::create("/home/sesank/.tasks").unwrap();
-		for task in &self.todos {
-			let _ = file.write_fmt(format_args!("{},{}\n",task.text.replace("\n", ""), task.completed as i32));
-		}
-	}
-}
-
-fn get_input() -> String {
-	let stdin = stdin();
-	let mut foo = String::new();
-	println!("\n\n{}{}New task name{}: ", color::Fg(color::Black), color::Bg(color::White), termion::style::Reset);
-	let _ = stdin.read_line(&mut foo);
-	foo
-}
-
-fn show_controls(num_items: i32) {
-	let mut row = 10;
-	if num_items > 8 {
-		row = num_items + 4;
-	}
-
-	println!("{}{} <a> add task \t <c> mark/unmark \t <q> quit {}",
-			 termion::cursor::Goto(1, row as u16),
-			 color::Fg(color::Yellow),
-			 termion::style::Reset);
-	println!("{}{} <q> quit \t <d> delete task \t <j>/<k> navigate {} ",
-			 termion::cursor::Goto(1, (row + 1) as u16),
-			 color::Fg(color::Yellow),
-			 termion::style::Reset);
-}
+mod task;
 
 fn main() {
 	let stdin = stdin();
 	let mut stdout = stdout().into_raw_mode().unwrap();
 
-	let mut todo_list = read_file();
+	let mut todo_list = task::read_file();
 	todo_list.display();
 
 	for c in stdin.events() {
@@ -169,13 +18,9 @@ fn main() {
 			Event::Key(Key::Char('q')) => break,
 			Event::Key(Key::Char('j')) => todo_list.inc(),
 			Event::Key(Key::Char('k')) => todo_list.dec(),
-			Event::Key(Key::Char('d')) => todo_list.remove_selected(),
+			Event::Key(Key::Char('d')) => todo_list.del(),
 			Event::Key(Key::Char('c')) => todo_list.todos[todo_list.selected as usize].toggle_status(),
-			Event::Key(Key::Char('a')) => {
-				let _ = RawTerminal::suspend_raw_mode(&stdout);
-				todo_list.add_task(Task::new(get_input(), false));
-				let _ = RawTerminal::activate_raw_mode(&stdout);
-			},
+			Event::Key(Key::Char('a')) => todo_list.add(&mut stdout),
 			_ => {}
 		}
 		stdout.flush().unwrap();
---
+++ src/task.rs
@@ -0,0 +1,165 @@
+use std::fs;
+use std::fs::File;
+use std::io::{stdin, stdout, Stdout, Write};
+use std::str::Split;
+
+use termion::color;
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
+pub fn read_file() -> TaskList {
+	let mut tl: TaskList = TaskList::new();
+	let contents = fs::read_to_string("/home/sesank/.tasks");
+	match contents {
+		Err(_) => return tl,
+		_ => {}
+	}
+
+	let contents = contents.unwrap();
+	let tasks_raw: Split<&str> = contents.split("\n");
+	for task in tasks_raw {
+		let mut task_details: Split<&str> = task.split(",");
+		let text = task_details.next();
+		let completion = task_details.next();
+		if text == None || completion == None {
+			break;
+		}
+		tl._add(text.unwrap().to_string(), completion.unwrap().to_string() == "1".to_string());
+	}
+	tl
+}
+
+pub fn show_controls(num_items: i32) {
+	let mut row = 10;
+	if num_items > 8 {
+		row = num_items + 4;
+	}
+
+	println!("{}{} <a> add task \t <c> mark/unmark \t <q> quit {}",
+			 termion::cursor::Goto(1, row as u16),
+			 color::Fg(color::Yellow),
+			 termion::style::Reset);
+	println!("{}{} <q> quit \t <d> delete task \t <j>/<k> navigate {} ",
+			 termion::cursor::Goto(1, (row + 1) as u16),
+			 color::Fg(color::Yellow),
+			 termion::style::Reset);
+}
+
+pub struct Task {
+	text: String,
+	completed: bool
+}
+
+impl Task {
+	pub fn new(name: String, completed: bool) -> Task {
+		Task {
+			text: name,
+			completed: completed
+		}
+	}
+
+	pub fn toggle_status(&mut self) {
+		self.completed = !self.completed;
+	}
+
+	pub fn display(&self, row:i32, selected:bool) {
+		match self.completed {
+			true => print!("[X] "),
+			false => print!("[ ] ")
+		}
+		match selected {
+			true => print!("{}{}", color::Fg(color::Black), color::Bg(color::White)),
+			false => print!("{}{}", color::Fg(color::White), color::Bg(color::Black))
+		}
+		println!("{}{}{}",
+				 self.text,
+				 termion::style::Reset,
+				 termion::cursor::Goto(1, (row + 1) as u16))
+	}
+}
+
+pub struct TaskList {
+	pub selected: i32,
+	pub todos: Vec<Task>
+}
+
+impl TaskList {
+	pub fn new() -> TaskList {
+		TaskList {
+			selected: 0,
+			todos: Vec::new()
+		}
+	}
+
+	pub fn display(&self) {
+		print!("{}", termion::clear::All);
+		print!("{}", termion::cursor::Hide);
+		print!("{}", termion::cursor::Goto(1, 1));
+
+		if self.todos.len() as i32 == 0 {
+			println!("No todo items.");
+		}
+		for i in 0..self.todos.len() as i32 {
+			self.todos[i as usize].display(i, i == self.selected);
+		}
+
+		show_controls(self.todos.len() as i32);
+	}
+
+	pub fn _add(&mut self, name: String, status: bool) {
+		self.todos.push(Task::new(name, status));
+	}
+
+	pub fn add<W: std::io::Write>(&mut self, stdout: &mut RawTerminal<W>) {
+		let stdin = stdin();
+		let _ = RawTerminal::suspend_raw_mode(&stdout);
+
+		let mut new_task = String::new();
+		println!("\n\n{}{}New task name{}: ",
+				 color::Fg(color::Black),
+				 color::Bg(color::White),
+				 termion::style::Reset);
+
+		let _ = stdin.read_line(&mut new_task);
+		let _ = RawTerminal::activate_raw_mode(&stdout);
+
+		self._add(new_task, false);
+	}
+
+	pub fn del(&mut self){
+		if self.todos.len() > 0 {
+			self.todos.remove(self.selected as usize);
+		}
+		if self.todos.len() == 0 {
+			return;
+		}
+		else if (self.selected as usize) >= self.todos.len() {
+			self.selected = (self.todos.len() - 1) as i32;
+		}
+	}
+
+	pub fn inc(&mut self) {
+		if self.todos.len() as i32 == 0 {
+			return;
+		}
+		if self.selected != (self.todos.len() - 1) as i32 {
+			self.selected += 1;
+		}
+	}
+
+	pub fn dec(&mut self){
+		if self.selected != 0 {
+			self.selected -= 1;
+		}
+	}
+
+	pub fn dump(&self){
+		let mut file = File::create("/home/sesank/.tasks").unwrap();
+		for task in &self.todos {
+			let _ = file.write_fmt(format_args!("{},{}\n",
+												task.text.replace("\n", ""),
+												task.completed as i32));
+		}
+	}
+}
